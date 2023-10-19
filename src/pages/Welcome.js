6da81df45096d24b837e328648ef777bdceb0748
Hari-Bombon <haripriya14022003@gmail.com>
2023-10-19 23:10:56 +0530

    Added the welcome component and css

---
+++ src/pages/Welcome.js
@@ -0,0 +1,10 @@
+import React from "react";
+import style from '../styles/welcome.module.css'
+
+
+export default function Welcome() {
+    return 
+       <div className={style.welcomeContainer}></div>;
+   
+}
+
